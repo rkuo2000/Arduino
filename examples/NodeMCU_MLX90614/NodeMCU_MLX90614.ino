67438435446b9f55a4953c78617face5d6bf5213
Richard Kuo <rkuo2000@gmail.com>
2018-12-11 23:03:17 +0800

    Add files via upload

--- examples/NodeMCU_MLX90614/NodeMCU_MLX90614.ino
+++ examples/NodeMCU_MLX90614/NodeMCU_MLX90614.ino
@@ -1,1 +1,47 @@
+/*************************************************** 
+  This is a library example for the MLX90614 Temp Sensor
 
+  Designed specifically to work with the MLX90614 sensors in the
+  adafruit shop
+  ----> https://www.adafruit.com/products/1748
+  ----> https://www.adafruit.com/products/1749
+
+  These sensors use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Adafruit MLX90614 test");
+  mlx.begin();
+}
+
+void loop() {
+// first MLX90614 sensor
+  Wire.begin(D2, D1);  
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.print("*C");
+//  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
+//  Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
+//  Serial.println();
+
+// second MLX90614 sensor
+  Wire.begin(D4, D3);
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+//  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
+//  Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
+//  Serial.println();
+  delay(500);  
+}
