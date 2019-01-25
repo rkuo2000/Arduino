153c9d0f11b431f8cad85d776b52db7fbefbbc47
Richard Kuo <rkuo2000@gmail.com>
2019-01-25 18:38:46 +0800

    Update MLX90614.ino

--- examples/MLX90614/MLX90614.ino
+++ examples/MLX90614/MLX90614.ino
@@ -1,22 +1,6 @@
-/*************************************************** 
-  This is a library example for the MLX90614 Temp Sensor
-
-  Designed specifically to work with the MLX90614 sensors in the
-  adafruit shop
-  ----> https://www.adafruit.com/products/1748
-  ----> https://www.adafruit.com/products/1749
-
-  These sensors use I2C to communicate, 2 pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
+// use Arafruit_MLX90614 driver code
 // NodeMCU use 3 pairs of I2C pins for three MLX90614 temp sensors
+
 #include <Wire.h>
 #include "Adafruit_MLX90614.h"
 
