a28550be655f63c039069522d76d3cb2ab6ed0fd
Richard Kuo <rkuo2000@gmail.com>
2019-09-04 19:00:56 +0800

    Add files via upload

--- examples/NodeMCU_Webclient_DHT11/NodeMCU_Webclient_DHT11.ino
+++ examples/NodeMCU_Webclient_DHT11/NodeMCU_Webclient_DHT11.ino
@@ -1,6 +1,8 @@
 // Webclient to read DHT11 and send data to Webserver
 #include <ESP8266WiFi.h>
-#include "DHT.h"
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <DHT.h>
 
 #define DHTPIN 12     // NodeMCU pin D6 connected to DHT11 pin Data
 
@@ -11,27 +13,12 @@
 
 DHT dht(DHTPIN, DHTTYPE, 15);
 
-const char* ssid     = "Home_SSID";
-const char* password = "Home_Password";
-const char* webserverIP = "http://192.168.1.88"
-
-void turnOff(int pin) {
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, 1);
-}
+const char* ssid     = "Kuo";
+const char* password = "0972211921";
+String      webserverIP = "http://192.168.1.5";
 
 void setup() {
   Serial.begin(115200);
-
-  // disable all output to save power
-  turnOff(0);
-  turnOff(2);
-  turnOff(4);
-  turnOff(5);
-  turnOff(12);
-  turnOff(13);
-  turnOff(14);
-  turnOff(15);
 
   dht.begin();
   delay(10);
@@ -57,26 +44,29 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-int value = 0;
-
 void loop() {
   delay(5000);
-  ++value;
 
-  String temp = String(dht.readTemperature());
-  String humidity = String(dht.readHumidity());
-  String voltage = String(system_get_free_heap_size());
+  float temp = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  Serial.print(temp);
+  Serial.print(" ");
+  Serial.print(humidity);
+  Serial.println();
+  
   String url = webserverIP + "/dht11?";
   url += "T=";
-  url += temp;
+  url += String(temp);
   url += "&H=";
-  url += humidity;
+  url += String(humidity);
   
   if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
- 
+
+     WiFiClient client;
+
      HTTPClient http;    //Declare object of class HTTPClient
  
-     http.begin(url);    //Specify request destination
+     http.begin(client, url);    //Specify request destination
      http.addHeader("Content-Type", "text/plain");  //Specify content-type header
  
      int httpCode = http.POST("Message from ESP8266");   //Send the request
@@ -93,7 +83,6 @@ void loop() {
   
   Serial.println();
   Serial.println("closing connection. going to sleep...");
-  delay(1000);
   // go to deepsleep for 1 minutes
   //system_deep_sleep_set_option(0);
   //system_deep_sleep(1 * 60 * 1000000);
