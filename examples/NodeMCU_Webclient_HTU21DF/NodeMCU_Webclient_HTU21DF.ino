3017b9e1dc6818206e27749c6090d1b5ba0c37a6
Richard Kuo <rkuo2000@gmail.com>
2019-09-04 19:04:22 +0800

    Add files via upload

--- examples/NodeMCU_Webclient_HTU21DF/NodeMCU_Webclient_HTU21DF.ino
+++ examples/NodeMCU_Webclient_HTU21DF/NodeMCU_Webclient_HTU21DF.ino
@@ -1,1 +1,89 @@
+// Webclient to read HTU21DF and send data to Webserver
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <Wire.h>
+#include <Adafruit_HTU21DF.h>
 
+// Connect Vin to 3-5VDC
+// Connect GND to ground
+// Connect SCL to I2C clock pin (A5 on UNO, D1 on NodeMCU)
+// Connect SDA to I2C data pin (A4 on UNO, D2 on NodeMCU)
+
+Adafruit_HTU21DF htu = Adafruit_HTU21DF();
+
+const char* ssid     = "Kuo";
+const char* password = "0972211921";
+String      webserverIP = "http://192.168.1.5";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("HTU21D-F test");  
+  if (!htu.begin()) {
+    Serial.println("Couldn't find HTU21DF sensor!");
+    while (1);
+  }
+  
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  delay(5000);
+
+  float temp  = htu.readTemperature();
+  float humid = htu.readHumidity();
+  Serial.print(temp);
+  Serial.print(" ");
+  Serial.print(humid);
+  Serial.println();
+  
+  String url = webserverIP + "/htu21?";
+  url += "T=";
+  url += String(temp);
+  url += "&H=";
+  url += String(humid);
+  
+  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+
+     WiFiClient client;
+
+     HTTPClient http;    //Declare object of class HTTPClient
+ 
+     http.begin(client, url);    //Specify request destination
+     http.addHeader("Content-Type", "text/plain");  //Specify content-type header
+ 
+     int httpCode = http.POST("Message from ESP8266");   //Send the request
+     String payload = http.getString();                  //Get the response payload
+ 
+     Serial.println(httpCode);   //Print HTTP return code
+     Serial.println(payload);    //Print request response payload
+
+     http.end();  //Close connection
+ 
+   }else{
+      Serial.println("Error in WiFi connection");   
+   }
+  
+  Serial.println();
+  Serial.println("closing connection. going to sleep...");
+  // go to deepsleep for 1 minutes
+  //system_deep_sleep_set_option(0);
+  //system_deep_sleep(1 * 60 * 1000000);
+  delay(1*60*1000);
+}
