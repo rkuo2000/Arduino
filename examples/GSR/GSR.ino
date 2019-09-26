0d107c0d339680c3ccc01a0fd58a619f93f9d6f1
Richard Kuo <rkuo2000@gmail.com>
2019-09-27 06:10:11 +0800

    Add files via upload

--- examples/GSR/GSR.ino
+++ examples/GSR/GSR.ino
@@ -1,1 +1,42 @@
+# GSR - Galvanic Skin Response
+const int BUZZER = D0;
+const int GSR = A2;
+int threshold = 0;
+int sensorValue;
 
+void setup(){
+  long sum=0;
+  Serial.begin(9600);
+  pinMode(BUZZER,OUTPUT);
+  digitalWrite(BUZZER,LOW);
+  delay(1000);
+  
+  for(int i=0;i<500;i++){
+    sensorValue=analogRead(GSR);
+    sum += sensorValue;
+    delay(5);
+  }
+  threshold = sum/500;
+  Serial.print("threshold =");
+  Serial.println(threshold);
+}
+
+void loop(){
+  int temp;
+  sensorValue=analogRead(GSR);
+  Serial.print("sensorValue=");
+  Serial.println(sensorValue);
+  temp = threshold - sensorValue;
+  if(abs(temp)>50)
+  {
+    sensorValue=analogRead(GSR);
+    temp = threshold - sensorValue;
+    if(abs(temp)>50){
+      digitalWrite(BUZZER,HIGH);
+      Serial.println("YES!");
+      delay(3000);
+      digitalWrite(BUZZER,LOW);
+      delay(1000);
+	}
+  }
+}
