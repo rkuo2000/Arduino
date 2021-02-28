f4779f3608d8acfc29875ed8f135d92498047221
Richard Kuo <rkuo2000@gmail.com>
2021-02-28 18:24:00 +0800

    Add files via upload

--- examples/ESP32_DAC_Audio/ESP32_DAC_Audio.ino
+++ examples/ESP32_DAC_Audio/ESP32_DAC_Audio.ino
@@ -1,1 +1,33 @@
+;l// Playing a digital WAV recording repeatadly using the XTronical DAC Audio library
+// prints out to the serial monitor numbers counting up showing that the sound plays 
+// independently of the main loop
+// See www.xtronical.com for write ups on sound
+// You will obviously need the appropriate hardware such as ESP32, audio amp and speaker, see 
+// http://www.xtronical.com/basics/audio/dacs-on-esp32/ for the simple build and specifically
+// 
+// for the write ip on digitised speach/ sounds
 
+// Modification to support SRAM reduction by using const unsigned char *WavData
+// XT_DAC_Audio.cpp : XT_Wav_Class::XT_Wav_Class(const unsigned char *WavData)
+// XT_DAC_Audio.h   : XT_Wav_Class(const unsigned char *WavData);
+
+#include "SoundData.h";
+#include "XT_DAC_Audio.h";
+
+XT_Wav_Class ForceWithYou(SoundData); // create an object of type XT_Wav_Class that is used by 
+                                      // the dac audio class (below), passing wav data as parameter.
+                                      
+XT_DAC_Audio_Class DacAudio(25,0);    // Create the main player class object. 
+                                      // Use GPIO 25, one of the 2 DAC pins and timer 0
+
+void setup() {
+  Serial.begin(115200); 
+}
+
+void loop() {
+  static uint32_t i=0;                // simple counter to output
+  if(ForceWithYou.Completed)          // if completed playing, play again
+    DacAudio.PlayWav(&ForceWithYou);  // play the wav (pass the wav class object created at top of code
+  Serial.println(i);                  // print out the value of i
+  i++;                                // increment the value of i
+}
