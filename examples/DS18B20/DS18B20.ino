cbc3734e3828d2a6abec3498f6625b1d4fd5ccbe
Richard Kuo <rkuo2000@gmail.com>
2021-03-08 23:44:58 +0800

    Add files via upload

--- examples/DS18B20/DS18B20.ino
+++ examples/DS18B20/DS18B20.ino
@@ -1,1 +1,22 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+// Connection
+#define ONE_WIRE_BUS 4 // NodeMCU: D4, NodeMCU-32S: 4 with 1K ohm pullup
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println("Temperature Sensor");
+  sensors.begin();
+}
+
+void loop(void)
+{
+  sensors.requestTemperatures(); // Send the command to get temperature reading
+  Serial.println(sensors.getTempCByIndex(0));
+  delay(1000);
+}
