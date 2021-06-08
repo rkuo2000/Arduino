5c58f170c44af9d00451728c210ccd4aaf7292be
Richard Kuo <rkuo2000@gmail.com>
2021-06-08 18:12:41 +0800

    Add files via upload

--- examples/ESP32_Audio_DAC_PlayFLAC/ESP32_Audio_DAC_PlayFLAC.ino
+++ examples/ESP32_Audio_DAC_PlayFLAC/ESP32_Audio_DAC_PlayFLAC.ino
@@ -1,1 +1,32 @@
+#include <Arduino.h>
+#include <AudioOutputI2S.h>
+#include <AudioFileSourcePROGMEM.h>
+#include <AudioGeneratorFLAC.h>
 
+#include "sample.h"
+
+AudioOutputI2S *out;
+AudioFileSourcePROGMEM *file;
+AudioGeneratorFLAC *flac;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Play FLAC from PROGMEM...\n");
+
+  audioLogger = &Serial;
+  file = new AudioFileSourcePROGMEM( sample_flac, sizeof(sample_flac) );
+  out = new AudioOutputI2S(0, 1); // port=0, output_mode = INTERNAL_DAC
+  flac = new AudioGeneratorFLAC();
+  flac->begin(file, out);
+}
+
+void loop()
+{
+  if (flac->isRunning()) {
+    if (!flac->loop()) flac->stop();
+  } else {
+    Serial.printf("FLAC done\n");
+    delay(1000);
+  }
+}
