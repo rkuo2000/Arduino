05efb4423fed4da6853f078b47403d343b5697ee
Richard Kuo <rkuo2000@gmail.com>
2021-11-08 18:36:46 +0800

    Update ESP32_MPU6050_ACC2CSV.ino

--- examples/ESP32_MPU6050_ACC2CSV/ESP32_MPU6050_ACC2CSV.ino
+++ examples/ESP32_MPU6050_ACC2CSV/ESP32_MPU6050_ACC2CSV.ino
@@ -4,7 +4,10 @@
 // I2C to MPU6050 (3-axis accelerometer & 3-axis gyroscope)
 
 #include <WebSerial.h>
-#include <Wire.h>
+#include "I2Cdev.h" 
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
 #include <MPU6050_6Axis_MotionApps20.h>
 
 MPU6050 imu;
