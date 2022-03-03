5ede1eadd578bf9408db33ae66fc1b8ccb6a3b4b
Richard Kuo <rkuo2000@gmail.com>
2022-03-03 19:50:55 +0800

    Add files via upload

---
+++ attachInterrupt.ino
@@ -0,0 +1,17 @@
+const byte ledPin = 2;       // Builtin-LED pin
+const byte interruptPin = 0; // BOOT/IO0 button pin
+volatile byte state = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+}
+
+void loop() {
+  digitalWrite(ledPin, state);
+}
+
+void blink() {
+  state = !state;
+}
