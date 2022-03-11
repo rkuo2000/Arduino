ec83bc5c3fa88fa9187e2f194870ac33cd3d1537
Richard Kuo <rkuo2000@gmail.com>
2022-03-11 17:15:21 +0800

    Add files via upload

--- examples/VL53L0X_HCSR04/VL53L0X_HCSR04.ino
+++ examples/VL53L0X_HCSR04/VL53L0X_HCSR04.ino
@@ -1,1 +1,41 @@
+#include "Adafruit_VL53L0X.h"
+#include <afstandssensor.h>
 
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+AfstandsSensor afstandssensor(13, 12);
+
+void setup() {
+  Serial.begin(115200);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+}
+
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+
+  Serial.print("HCSR04 (mm): "); Serial.print(afstandssensor.afstandCM());
+  Serial.print("\t");
+     
+  //Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("VL53L0X (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+    
+  //delay(100);
+}
