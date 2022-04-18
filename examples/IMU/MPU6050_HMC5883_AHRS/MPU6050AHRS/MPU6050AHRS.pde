cf24c367d0cbc3d35606983528c1778e8255ee31
Richard Kuo <rkuo2000@gmail.com>
2022-04-18 14:37:11 +0800

    Add files via upload

---
+++ examples/IMU/MPU6050_HMC5883_AHRS/MPU6050AHRS/MPU6050AHRS.pde
@@ -0,0 +1,171 @@
+//
+// Processign 4.0b7 to add Toxi library
+//
+import processing.serial.*;
+import processing.opengl.*;
+import toxi.geom.*;  
+import toxi.processing.*;
+
+ToxiclibsSupport gfx;
+
+Serial port;                         // The serial port
+char[] teapotPacket = new char[14];  // InvenSense Teapot packet
+int serialCount = 0;                 // current packet byte position
+int aligned = 0;
+int interval = 0;
+
+float[] q = new float[4];
+Quaternion quat = new Quaternion(1, 0, 0, 0);
+
+float[] gravity = new float[3];
+float[] euler = new float[3];
+float[] ypr = new float[3];
+
+void setup() {
+    // 300px square viewport using P3D rendering
+    size(300, 300, P3D); 
+    noStroke(); 
+    colorMode(RGB, 1);
+    
+    gfx = new ToxiclibsSupport(this);
+  
+    // display serial port list for debugging/clarity
+    println(Serial.list());
+
+    String portName = Serial.list()[0];
+    //String portName = "COM4";
+    //String portName = "/dev/ttyUSB0"; // for Linux	
+    println("connect to ",portName);
+
+    port = new Serial(this, portName, 9600);
+    
+    //port.write('r'); // send a character to trigger MPU6050
+}
+
+void draw() {
+/*
+    // resend a character to trigger MPU6050 in the case it halts
+    if (millis() - interval > 1000) {
+        port.write('r');
+        interval = millis();
+    }
+*/    
+    // grey background
+    background(0.5); //0 = black
+    
+    // translate everything to the middle of the viewport
+    pushMatrix();
+    translate(width / 2, height / 2);
+
+    // 3-step rotation from yaw/pitch/roll angles
+    //rotateY(-ypr[0]); // yaw
+    //rotateZ(-ypr[1]); // pitch
+    //rotateX(-ypr[2]); // roll
+
+    // toxiclibs direct angle/axis rotation from quaternion (NO gimbal lock!)
+    // (axis order [1, 3, 2] and inversion [-1, +1, +1] is a consequence of
+    // different coordinate system orientation assumptions between Processing
+    // and InvenSense DMP)
+    float[] axis = quat.toAxisAngle();
+    rotate(axis[0], -axis[1], axis[3], axis[2]);
+
+    scale(50);
+    beginShape(QUADS);
+    fill(0, 1, 1); vertex(-1,  1,  1);
+    fill(1, 1, 1); vertex( 1,  1,  1);
+    fill(1, 0, 1); vertex( 1, -1,  1);
+    fill(0, 0, 1); vertex(-1, -1,  1);
+
+    fill(1, 1, 1); vertex( 1,  1,  1);
+    fill(1, 1, 0); vertex( 1,  1, -1);
+    fill(1, 0, 0); vertex( 1, -1, -1);
+    fill(1, 0, 1); vertex( 1, -1,  1);
+
+    fill(1, 1, 0); vertex( 1,  1, -1);
+    fill(0, 1, 0); vertex(-1,  1, -1);
+    fill(0, 0, 0); vertex(-1, -1, -1);
+    fill(1, 0, 0); vertex( 1, -1, -1);
+
+    fill(0, 1, 0); vertex(-1,  1, -1);
+    fill(0, 1, 1); vertex(-1,  1,  1);
+    fill(0, 0, 1); vertex(-1, -1,  1);
+    fill(0, 0, 0); vertex(-1, -1, -1);
+
+    fill(0, 1, 0); vertex(-1,  1, -1);
+    fill(1, 1, 0); vertex( 1,  1, -1);
+    fill(1, 1, 1); vertex( 1,  1,  1);
+    fill(0, 1, 1); vertex(-1,  1,  1);
+
+    fill(0, 0, 0); vertex(-1, -1, -1);
+    fill(1, 0, 0); vertex( 1, -1, -1);
+    fill(1, 0, 1); vertex( 1, -1,  1);
+    fill(0, 0, 1); vertex(-1, -1,  1);
+    endShape();
+
+    popMatrix();
+}
+
+void serialEvent(Serial port) {
+    interval = millis();
+    while (port.available() > 0) {
+        int ch = port.read();
+        print((char)ch);
+        if (ch == '$') {serialCount = 0;} // this will help with alignment
+        if (aligned < 4) {
+            // make sure we are properly aligned on a 14-byte packet
+            if (serialCount == 0) {
+                if (ch == '$') aligned++; else aligned = 0;
+            } else if (serialCount == 1) {
+                if (ch == 2) aligned++; else aligned = 0;
+            } else if (serialCount == 12) {
+                if (ch == '\r') aligned++; else aligned = 0;
+            } else if (serialCount == 13) {
+                if (ch == '\n') aligned++; else aligned = 0;
+            }
+            //println(ch + " " + aligned + " " + serialCount);
+            serialCount++;
+            if (serialCount == 14) serialCount = 0;
+        } else {
+            if (serialCount > 0 || ch == '$') {
+                teapotPacket[serialCount++] = (char)ch;
+                if (serialCount == 14) {
+                    serialCount = 0; // restart packet byte position
+                    
+                    // get quaternion from data packet
+                    q[0] = ((teapotPacket[2] << 8) | teapotPacket[3]) / 16384.0f;
+                    q[1] = ((teapotPacket[4] << 8) | teapotPacket[5]) / 16384.0f;
+                    q[2] = ((teapotPacket[6] << 8) | teapotPacket[7]) / 16384.0f;
+                    q[3] = ((teapotPacket[8] << 8) | teapotPacket[9]) / 16384.0f;
+                    for (int i = 0; i < 4; i++) if (q[i] >= 2) q[i] = -4 + q[i];
+                    
+                    // set our toxilibs quaternion to new data
+                    quat.set(q[0], q[1], q[2], q[3]);
+
+                    /*
+                    // below calculations unnecessary for orientation only using toxilibs
+                    
+                    // calculate gravity vector
+                    gravity[0] = 2 * (q[1]*q[3] - q[0]*q[2]);
+                    gravity[1] = 2 * (q[0]*q[1] + q[2]*q[3]);
+                    gravity[2] = q[0]*q[0] - q[1]*q[1] - q[2]*q[2] + q[3]*q[3];
+        
+                    // calculate Euler angles
+                    euler[0] = atan2(2*q[1]*q[2] - 2*q[0]*q[3], 2*q[0]*q[0] + 2*q[1]*q[1] - 1);
+                    euler[1] = -asin(2*q[1]*q[3] + 2*q[0]*q[2]);
+                    euler[2] = atan2(2*q[2]*q[3] - 2*q[0]*q[1], 2*q[0]*q[0] + 2*q[3]*q[3] - 1);
+        
+                    // calculate yaw/pitch/roll angles
+                    ypr[0] = atan2(2*q[1]*q[2] - 2*q[0]*q[3], 2*q[0]*q[0] + 2*q[1]*q[1] - 1);
+                    ypr[1] = atan(gravity[0] / sqrt(gravity[1]*gravity[1] + gravity[2]*gravity[2]));
+                    ypr[2] = atan(gravity[1] / sqrt(gravity[0]*gravity[0] + gravity[2]*gravity[2]));
+                    
+                    // output various components for debugging
+                    //println("q:\t" + round(q[0]*100.0f)/100.0f + "\t" + round(q[1]*100.0f)/100.0f + "\t" + round(q[2]*100.0f)/100.0f + "\t" + round(q[3]*100.0f)/100.0f);
+                    //println("euler:\t" + euler[0]*180.0f/PI + "\t" + euler[1]*180.0f/PI + "\t" + euler[2]*180.0f/PI);
+                    //println("ypr:\t" + ypr[0]*180.0f/PI + "\t" + ypr[1]*180.0f/PI + "\t" + ypr[2]*180.0f/PI);
+                    */
+                }
+            }
+        }
+    }
+}
---
+++ examples/IMU/MPU6050_KalmanFilter/MPU6050_KalmanFilter.ino
@@ -0,0 +1,177 @@
+/* Copyright (C) 2012 Kristian Lauszus, TKJ Electronics. All rights reserved.
+ This software may be distributed and modified under the terms of the GNU
+ General Public License version 2 (GPL2) as published by the Free Software
+ Foundation and appearing in the file GPL2.TXT included in the packaging of
+ this file. Please note that GPL2 Section 2[b] requires that all works based
+ on this software must also be made publicly available under the terms of
+ the GPL2 ("Copyleft").
+ Contact information
+ -------------------
+ Kristian Lauszus, TKJ Electronics
+ Web      :  http://www.tkjelectronics.com
+ e-mail   :  kristianl@tkjelectronics.com
+ */
+ 
+#include "Kalman.h" // Source: https://github.com/TKJElectronics/KalmanFilter
+#include "I2Cdev.h" 
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+#include "MPU6050.h"
+// nodeMCU I2C default SCL : D1, SDA : D2
+//#define sda 14 // NodeMCU pin D5
+//#define scl 12 // NodeMCU pin D6
+
+#define RESTRICT_PITCH // Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
+
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+
+/* IMU Data */
+MPU6050 imu;
+int16_t accX, accY, accZ;
+int16_t gyroX, gyroY, gyroZ;
+
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+
+uint32_t timer;
+uint8_t i2cData[14]; // Buffer for I2C data
+
+// TODO: Make calibration routine
+
+void setup() {
+  Serial.begin(115200);
+  //Wire.begin(sda, scl);
+  Wire.begin();
+#if ARDUINO >= 157
+  Wire.setClock(400000UL); // Set I2C frequency to 400kHz
+#else
+  TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
+#endif
+
+  imu.initialize();
+
+  delay(100); // Wait for sensor to stabilize
+
+  /* Set kalman and gyro starting angle */
+  imu.getAcceleration(&accX, &accY, &accZ);
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  kalmanX.setAngle(roll); // Set starting angle
+  kalmanY.setAngle(pitch);
+  gyroXangle = roll;
+  gyroYangle = pitch;
+  compAngleX = roll;
+  compAngleY = pitch;
+
+  timer = micros();
+}
+
+void loop() {
+  /* Update IMU sensor values */
+  imu.getAcceleration(&accX, &accY, &accZ);
+  imu.getRotation(&gyroX, &gyroY, &gyroZ);
+  
+  double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+  timer = micros();
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  double gyroXrate = gyroX / 131.0; // Convert to deg/s
+  double gyroYrate = gyroY / 131.0; // Convert to deg/s
+
+#ifdef RESTRICT_PITCH
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+    kalmanX.setAngle(roll);
+    compAngleX = roll;
+    kalAngleX = roll;
+    gyroXangle = roll;
+  } else
+    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleX) > 90)
+    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+#else
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
+    kalmanY.setAngle(pitch);
+    compAngleY = pitch;
+    kalAngleY = pitch;
+    gyroYangle = pitch;
+  } else
+    kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleY) > 90)
+    gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+#endif
+
+  gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
+  gyroYangle += gyroYrate * dt;
+  //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
+  //gyroYangle += kalmanY.getRate() * dt;
+
+  compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
+  compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+
+  // Reset the gyro angle when it has drifted too much
+  if (gyroXangle < -180 || gyroXangle > 180)
+    gyroXangle = kalAngleX;
+  if (gyroYangle < -180 || gyroYangle > 180)
+    gyroYangle = kalAngleY;
+
+  /* Print Data */
+#if 0 // Set to 1 to activate
+  Serial.print(accX); Serial.print("\t");
+  Serial.print(accY); Serial.print("\t");
+  Serial.print(accZ); Serial.print("\t");
+  Serial.print(gyroX); Serial.print("\t");
+  Serial.print(gyroY); Serial.print("\t");
+  Serial.print(gyroZ); Serial.print("\t");
+  Serial.print("\t");
+#endif
+
+  Serial.print(roll); Serial.print("\t");
+  Serial.print(gyroXangle); Serial.print("\t");
+  Serial.print(compAngleX); Serial.print("\t");
+  Serial.print(kalAngleX); Serial.print("\t");
+
+  Serial.print("\t");
+
+  Serial.print(pitch); Serial.print("\t");
+  Serial.print(gyroYangle); Serial.print("\t");
+  Serial.print(compAngleY); Serial.print("\t");
+  Serial.print(kalAngleY); Serial.print("\t");
+
+#if 0 // Set to 1 to print the temperature
+  Serial.print("\t");
+  double temperature = (double)tempRaw / 340.0 + 36.53;
+  Serial.print(temperature); Serial.print("\t");
+#endif
+
+  Serial.print("\r\n");
+  delay(2);
+}
