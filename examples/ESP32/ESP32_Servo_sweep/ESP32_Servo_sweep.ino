90cd2c27494b62625dcedb6687913c10a7572b62
Richard Kuo <rkuo2000@gmail.com>
2022-05-04 17:40:06 +0800

    Add files via upload

---
+++ examples/ESP32/ESP32_Servo_sweep/ESP32_Servo_sweep.ino
@@ -0,0 +1,33 @@
+#include <ESP32Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// 16 servo objects can be created on the ESP32
+
+int pos = 0;    // variable to store the servo position
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+// Possible PWM GPIO pins on the ESP32-S2: 0(used by on-board button),1-17,18(used by on-board LED),19-21,26,33-42
+#if defined(ARDUINO_ESP32S2_DEV)
+int servoPin = 17;
+#else
+int servoPin = 18;
+#endif
+
+void setup() {
+	// Allow allocation of all timers
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+	myservo.setPeriodHertz(50);    // standard 50 hz servo
+	myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
+	// using default min/max of 1000us and 2000us
+	// different servos may require different min/max settings
+	// for an accurate 0 to 180 sweep
+}
+
+void loop() {
+  for (pos=0; pos < 180; pos++) { // turn from degree 0 to 180
+    myservo.write(pos);    // tell servo to go to position in variable 'pos'
+    delay(15);
+  }
+}
