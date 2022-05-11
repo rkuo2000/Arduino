12c7c17c8531ab8a0c21a0b0beb2c586b0d89959
Richard Kuo <rkuo2000@gmail.com>
2022-05-12 01:44:35 +0800

    Add files via upload

---
+++ examples/ESP32/ESP32_CoAP_client/ESP32_CoAP_client.ino
@@ -0,0 +1,41 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <coap-simple.h>
+
+const char* ssid     = "CBN-42738-2.4G";
+const char* password = "0972211921";
+
+WiFiUDP udp;
+Coap coap(udp);
+
+bool LEDstate;
+int msgid;
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // start coap server/client
+  coap.start();
+
+  Serial.println("Send Request");
+  int msgid = coap.get(IPAddress(192,168,0,43), 5683, "time");  
+}
+
+void loop() {
+  Serial.println("Send Request");
+  if (LEDstate) 
+    msgid = coap.put(IPAddress(192,168,0,43), 5683, "light", "1");
+  else
+    msgid = coap.put(IPAddress(192,168,0,43), 5683, "light", "0");
+  LEDstate=!LEDstate;
+  delay(1000);
+}
