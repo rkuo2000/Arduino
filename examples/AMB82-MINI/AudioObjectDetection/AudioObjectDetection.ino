c268181d5bb84d582fdf7dd1a221e2940d832456
Richard Kuo <rkuo2000@gmail.com>
2023-12-13 23:26:11 +0800

    Add files via upload

---
+++ examples/AMB82-MINI/AudioObjectDetection/AudioObjectDetection.ino
@@ -0,0 +1,180 @@
+/*
+
+ Example guide:
+ https://www.amebaiot.com/en/amebapro2-arduino-neuralnework-object-detection/
+
+ NN Model Selection
+ Select Neural Network(NN) task and models using modelSelect(nntask, objdetmodel, facedetmodel, facerecogmodel).
+ Replace with NA_MODEL if they are not necessary for your selected NN Task.
+
+ NN task
+ =======
+ OBJECT_DETECTION/ FACE_DETECTION/ FACE_RECOGNITION
+
+ Models
+ =======
+ YOLOv3 model         DEFAULT_YOLOV3TINY   / CUSTOMIZED_YOLOV3TINY
+ YOLOv4 model         DEFAULT_YOLOV4TINY   / CUSTOMIZED_YOLOV4TINY
+ YOLOv7 model         DEFAULT_YOLOV7TINY   / CUSTOMIZED_YOLOV7TINY
+ SCRFD model          DEFAULT_SCRFD        / CUSTOMIZED_SCRFD
+ MobileFaceNet model  DEFAULT_MOBILEFACENET/ CUSTOMIZED_MOBILEFACENET
+ No model             NA_MODEL
+ */
+
+#include "WiFi.h"
+#include "StreamIO.h"
+#include "VideoStream.h"
+#include "RTSP.h"
+#include "NNObjectDetection.h"
+#include "VideoStreamOverlay.h"
+#include "ObjectClassList.h"
+#include "NNAudioClassification.h"
+#include "AudioClassList.h"
+
+#define CHANNEL 0
+#define CHANNELNN 3 
+
+// Lower resolution for NN processing
+#define NNWIDTH  576
+#define NNHEIGHT 320
+
+// NN audio classification requires 16KHz
+AudioSetting configA(16000, 1, USE_AUDIO_AMIC); // Sample rate, Channel count, Mic type
+Audio audio;
+NNAudioClassification audioNN;
+StreamIO audioStreamerNN(1, 1);                 // 1 Input Audio -> 1 Output Audio Classification
+
+VideoSetting config(VIDEO_FHD, 30, VIDEO_H264, 0);
+VideoSetting configNN(NNWIDTH, NNHEIGHT, 10, VIDEO_RGB, 0);
+NNObjectDetection ObjDet;
+RTSP rtsp;
+StreamIO videoStreamer(2, 2);
+StreamIO videoStreamerNN(2, 2);
+
+char ssid[] = "CBN-42738-2.4G";   // your network SSID (name)
+char pass[] = "0972211921";       // your network password
+int status = WL_IDLE_STATUS;
+
+IPAddress ip;
+int rtsp_portnum;
+
+void setup() {
+    Serial.begin(115200);
+
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to WPA SSID: ");
+        Serial.println(ssid);
+        status = WiFi.begin(ssid, pass);
+
+        // wait 2 seconds for connection:
+        delay(2000);
+    }
+    ip = WiFi.localIP();
+
+    // Configure camera video channels with video format information
+    // Adjust the bitrate based on your WiFi network quality
+    config.setBitrate(2 * 1024 * 1024);     // Recommend to use 2Mbps for RTSP streaming to prevent network congestion
+    Camera.configVideoChannel(CHANNEL, config);
+    Camera.configVideoChannel(CHANNELNN, configNN);
+    Camera.videoInit();
+
+    // Configure RTSP with corresponding video format information
+    rtsp.configVideo(config);
+    rtsp.begin();
+    rtsp_portnum = rtsp.getPort();
+
+    // Configure object detection with corresponding video format information
+    // Select Neural Network(NN) task and models
+    ObjDet.configVideo(configNN);
+    ObjDet.modelSelect(OBJECT_DETECTION, DEFAULT_YOLOV4TINY, NA_MODEL, NA_MODEL);
+    ObjDet.begin();
+
+    // Configure StreamIO object to stream data from video channel to RTSP
+    videoStreamer.registerInput(Camera.getStream(CHANNEL));
+    videoStreamer.registerOutput(rtsp);
+    if (videoStreamer.begin() != 0) {
+        Serial.println("StreamIO link start failed");
+    }
+
+    // Start data stream from video channel
+    Camera.channelBegin(CHANNEL);
+
+    // Configure StreamIO object to stream data from RGB video channel to object detection
+    videoStreamerNN.registerInput(Camera.getStream(CHANNELNN));
+    videoStreamerNN.setStackSize();
+    videoStreamerNN.setTaskPriority();
+    videoStreamerNN.registerOutput(ObjDet);
+    if (videoStreamerNN.begin() != 0) {
+        Serial.println("StreamIO link start failed");
+    }
+
+    // Start video channel for NN
+    Camera.channelBegin(CHANNELNN);
+
+    // Start OSD drawing on RTSP video channel
+    OSD.configVideo(CHANNEL, config);
+    OSD.begin();
+}
+
+void loop() {
+    std::vector<ObjectDetectionResult> results = ObjDet.getResult();
+
+    uint16_t im_h = config.height();
+    uint16_t im_w = config.width();
+
+    Serial.print("Network URL for RTSP Streaming: ");
+    Serial.print("rtsp://");
+    Serial.print(ip);
+    Serial.print(":");
+    Serial.println(rtsp_portnum);
+    Serial.println(" ");
+
+    printf("Total number of objects detected = %d\r\n", ObjDet.getResultCount());
+    OSD.createBitmap(CHANNEL);
+
+    if (ObjDet.getResultCount() > 0) {
+        for (uint32_t i = 0; i < ObjDet.getResultCount(); i++) {
+            int obj_type = results[i].type();
+            if (itemList[obj_type].filter) {    // check if item should be ignored
+
+                ObjectDetectionResult item = results[i];
+                // Result coordinates are floats ranging from 0.00 to 1.00
+                // Multiply with RTSP resolution to get coordinates in pixels
+                int xmin = (int)(item.xMin() * im_w);
+                int xmax = (int)(item.xMax() * im_w);
+                int ymin = (int)(item.yMin() * im_h);
+                int ymax = (int)(item.yMax() * im_h);
+
+                // Draw boundary box
+                printf("Item %d %s:\t%d %d %d %d\n\r", i, itemList[obj_type].objectName, xmin, xmax, ymin, ymax);
+                OSD.drawRect(CHANNEL, xmin, ymin, xmax, ymax, 3, OSD_COLOR_WHITE);
+
+                // Print identification text
+                char text_str[20];
+                snprintf(text_str, sizeof(text_str), "%s %d", itemList[obj_type].objectName, item.score());
+                OSD.drawText(CHANNEL, xmin, ymin - OSD.getTextHeight(CHANNEL), text_str, OSD_COLOR_CYAN);
+            }
+        }
+    }
+    OSD.update(CHANNEL);
+
+    // delay to wait for new results
+    delay(100);
+}
+
+// User callback function
+void ACPostProcess(std::vector<AudioClassificationResult> results) {
+    printf("No of Audio Detected = %d\r\n", audioNN.getResultCount());
+
+    if (audioNN.getResultCount() > 0) {
+        for (int i = 0; i < audioNN.getResultCount(); i++) {
+            AudioClassificationResult audio_item = results[i];
+            int class_id = (int)audio_item.classID();
+            if (audioNames[class_id].filter) {
+                int prob = audio_item.score();
+                printf("%d class %d, score: %d, audio name: %s\r\n", i, class_id, prob, audioNames[class_id].audioName);
+            }
+        }
+    }
+}
