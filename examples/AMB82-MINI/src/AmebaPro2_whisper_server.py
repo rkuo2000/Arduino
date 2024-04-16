70af38cec804e9d170b6e6a1bc30b7531c7e80b6
Richard Kuo <rkuo2000@gmail.com>
2024-04-16 16:50:41 +0800

    Add files via upload

---
+++ examples/AMB82-MINI/src/AmebaPro2_whisper_server.py
@@ -0,0 +1,53 @@
+## To run server: python AmebaPro2_whisper_server.py
+## To run client: RecordMP4_HTTP_Post_Audio.ino
+
+# pip install git+https://github.com/openai/whisper.git
+# pip install fastapi uvicorn
+# pip install nest-asyncio
+# pip install accelerate
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import Response
+from pydantic import BaseModel
+import uvicorn
+import base64
+
+import whisper
+ASR = whisper.load_model("base")
+
+import torch
+
+import nest_asyncio
+nest_asyncio.apply()
+
+app = FastAPI()
+
+class Base64Data(BaseModel):
+    base64_string: str
+
+@app.get("/")
+def home():
+    return Response("hello")
+
+@app.post("/audio")
+async def post_audio(data: Base64Data):
+    try:
+        #Decode the base64 string
+        decoded_data = base64.b64decode(data.base64_string)
+        
+        # print(decoded_data)
+        #Save the decoded data to an MP4 file
+        with open("output.mp4", "wb") as f:
+            f.write(decoded_data)
+      
+        # Whisper transcribe
+        result = ASR.transcribe("output.mp4")
+        print("ASR: "+result["text"])
+
+        return Response(result["text"])
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
