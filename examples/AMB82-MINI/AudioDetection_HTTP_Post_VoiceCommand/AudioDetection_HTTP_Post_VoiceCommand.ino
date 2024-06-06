2177df4f8e99cac7affde7d9ff8a7d5f9b04defd
Richard Kuo <rkuo2000@gmail.com>
2024-06-06 17:24:59 +0800

    Update AudioDetection_HTTP_Post_VoiceCommand.ino

--- examples/AMB82-MINI/AudioDetection_HTTP_Post_VoiceCommand/AudioDetection_HTTP_Post_VoiceCommand.ino
+++ examples/AMB82-MINI/AudioDetection_HTTP_Post_VoiceCommand/AudioDetection_HTTP_Post_VoiceCommand.ino
@@ -39,8 +39,8 @@ int max_prob = 0;
 
 #define FILENAME "TestRecordingAudioOnly.mp4"
 
-char ssid[] = "HITRON-DF90-5G";              // your network SSID (Home WiFi or Smartphone Hotspot)
-char pass[] = "0972211921";        // your network password
+char ssid[] = "Your_SSID";            // your network SSID (Home WiFi or Smartphone Hotspot)
+char pass[] = "Your_Password";        // your network password
 int status = WL_IDLE_STATUS;
 
 char server[] = "123.195.32.57";   // the server IP running HTTP server on PC
